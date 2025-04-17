071fd04c7f47220053a80f6b076bff9fc1b9c23d
Carl Broaden <carlbroaden@gmail.com>
2025-04-17 06:46:54 -0400

    space added for better visibility on search bar

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   return (
     <main className="flex-1 flex flex-col gap-6 px-4">
       <Hero />
-      <div className="flex flex-col gap-2 items-center mx-auto">
+      <div className="flex flex-col gap-2 items-center mx-auto mb-20">
         <h1 className="font-bold text-xl">Find Movies</h1>
         <SearchBar />
       </div>
