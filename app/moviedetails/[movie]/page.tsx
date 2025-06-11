f4a4e366a34c5f4914230fd16fe0d1553a9aa8a8
Carl Broaden <carlbroaden@gmail.com>
2025-06-11 03:08:48 -0400

    fixed moviedetails image shrinking on mobile devices

--- app/moviedetails/[movie]/page.tsx
+++ app/moviedetails/[movie]/page.tsx
@@ -15,15 +15,16 @@ export default async function MovieDetailsPage({params }: {params: Promise<{ mov
   const movie = await data.json();
   return (
     <div className="flex flex-col items-center gap-4 mx-auto max-w-3xl">
-      <div className="flex  gap-4 flex-row border-b-2 px-6 pb-6">
+      <div className="flex flex-col gap-4 sm:flex-row border-b-2 px-6 pb-6">
         <Image
+          priority
           className="mx-auto rounded-xl shadow-md shadow-black"
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
           width={200}
           height={325}
         />
-        <div className="flex flex-col my-auto gap-4">
+        <div className="flex flex-col my-auto text-center sm:text-left gap-4">
           <div className="flex flex-col gap-4">
             <h1 className="text-2xl font-semibold">{movie.title}</h1>
             <div className="flex flex-col gap-2">
@@ -33,13 +34,13 @@ export default async function MovieDetailsPage({params }: {params: Promise<{ mov
             <div className="flex flex-col gap-2">
               
               <h2 className="font-semibold">Average Rating</h2>
-              <div className="flex gap-1 items-center">
+              <div className="flex gap-1 justify-center sm:justify-start items-center">
                 <Star size={14} fill="gold"  />
                 <p className="tracking-widest">{movie.vote_average.toFixed(1)}</p>
               </div>
               
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 w-3/4 sm:w-full mx-auto">
               <h2 className="font-semibold">Genres</h2>
               <div className="flex flex-wrap gap-2">
                 <p>{movie.genres?.map((genre: { name: any; }) => genre.name).join(" / ")}</p>
